91f6a9f909dddb078ff9185c7b7a8996155a6d14
Victor Osuyak <vosuyak@vsapartners.com>
2017-06-26 11:27:44 -0500

    push

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,32 +1,8 @@
 export class MainController {
-  constructor ($timeout, webDevTec, toastr) {
-    'ngInject';
-
-    this.awesomeThings = [];
-    this.classAnimation = '';
-    this.creationDate = 1498487912725;
-    this.toastr = toastr;
-
-    this.activate($timeout, webDevTec);
+  constructor () {
+    
   }
-
-  activate($timeout, webDevTec) {
-    this.getWebDevTec(webDevTec);
-    $timeout(() => {
-      this.classAnimation = 'rubberBand';
-    }, 4000);
-  }
-
-  getWebDevTec(webDevTec) {
-    this.awesomeThings = webDevTec.getTec();
-
-    angular.forEach(this.awesomeThings, (awesomeThing) => {
-      awesomeThing.rank = Math.random();
-    });
-  }
-
-  showToastr() {
-    this.toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
-    this.classAnimation = '';
+  postMessage(){
+    console.log('post');
   }
 }
